4a55977d2d23e6a02f469f12e608dd1e65345b2f
boon-boon <leeby-wm24@student.tarc.edu.my>
2025-09-25 22:15:45 +0800

    Fix: Corrected typo in ManagePaymentScreen filename
    The file `ManagfePaymentScreen.kt` has been renamed to `ManagePaymentScreen.kt` to fix a typographical error.
    
    Additionally, unused import statements `androidx.compose.foundation.layout.fillMaxHeight`, `androidx.compose.foundation.layout.height`, and `androidx.compose.ui.text.style.TextOverflow` were removed from the file.

--- app/src/main/java/com/example/mad_assignment/ui/managepayment/ManagePaymentScreen.kt
+++ app/src/main/java/com/example/mad_assignment/ui/managepayment/ManagePaymentScreen.kt
@@ -7,10 +7,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -40,7 +38,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
